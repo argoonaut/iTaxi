5d5fd578da191f1fa3b73bb450d6e0cf0c3ac7b1
argonautts <sasha.kozin.2014alex444@ya.ru>
2023-03-01 12:21:08 +0300

    Initial Commit

---
+++ iTaxi/iTaxiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTaxiApp.swift
+//  iTaxi
+//
+//  Created by Alexandr Kozin on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct iTaxiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
