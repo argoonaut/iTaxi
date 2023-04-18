27065093093b5e82139dc41b56bd396306447ddb
argonautts <sasha.kozin.2014alex444@ya.ru>
2023-04-19 00:35:44 +0300

    View Navigation & Firebase Setup

--- iTaxi/Core/Authentication/Views/LoginView.swift
+++ iTaxi/Core/Authentication/Views/LoginView.swift
@@ -13,126 +13,129 @@ struct LoginView: View {
     @State var password = ""
     
     var body: some View {
-        ZStack {
-            Color(.black)
-                .ignoresSafeArea()
-            
-            VStack {
-                
-                // MARK: Image and title
-                // TODO: Replace uber logo on my logo
+        NavigationStack {
+            ZStack {
+                Color(.black)
+                    .ignoresSafeArea()
                 
                 VStack {
-                    // Image
-                    Image("uber-app-icon")
-                        .resizable()
-                        .frame(width: 200, height: 200)
                     
-                    // Title
-                    Text("iTaxi")
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                }
-                
-                // MARK: Input fields
-                
-                VStack(spacing: 32) {
-                    // Input field 1
-                    CustomInputFields(text: $email,
-                                      title: "Email Address",
-                                      placeholder: "name@example.com")
+                    // MARK: Image and title
+                    // TODO: Replace uber logo on my logo
                     
-                    // Input field 2
-                    CustomInputFields(text: $password,
-                                      title: "Password",
-                                      placeholder: "Enter your password",
-                                      isSecureField: true)
+                    VStack {
+                        // Image
+                        Image("uber-app-icon")
+                            .resizable()
+                            .frame(width: 200, height: 200)
+                        
+                        // Title
+                        Text("iTaxi")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                    }
                     
+                    // MARK: Input fields
+                    
+                    VStack(spacing: 32) {
+                        // Input field 1
+                        CustomInputFields(text: $email,
+                                          title: "Email Address",
+                                          placeholder: "name@example.com")
+                        
+                        // Input field 2
+                        CustomInputFields(text: $password,
+                                          title: "Password",
+                                          placeholder: "Enter your password",
+                                          isSecureField: true)
+                        
+                        Button {
+                            
+                        } label: {
+                            Text("Forgot Password ?")
+                                .font(.system(size: 13, weight: .semibold))
+                                .foregroundColor(.white)
+                                .padding(.top)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    .padding(.horizontal)
+                    .padding(.top, 12)
+                                    
+                    // MARK: Social sign in view
+                    VStack {
+                        // Dividers + text
+                        HStack(spacing: 24) {
+                            Rectangle()
+                                .frame(width: 76, height: 1)
+                                .foregroundColor(.white)
+                                .opacity(0.5)
+                            
+                            Text("Sign in with social")
+                                .foregroundColor(.white)
+                                .fontWeight(.semibold)
+                            
+                            Rectangle()
+                                .frame(width: 76, height: 1)
+                                .foregroundColor(.white)
+                                .opacity(0.5)
+                        }
+                        // Sign up buttons
+                        HStack(spacing: 24) {
+                            Button {
+                                
+                            } label: {
+                                Image("facebook-sign-in-icon")
+                                    .resizable()
+                                    .frame(width: 44, height: 44)
+                            }
+
+                            Button {
+                                
+                            } label: {
+                                Image("google-sign-in-icon")
+                                    .resizable()
+                                    .frame(width: 44, height: 44)
+                            }
+                        }
+                    }
+                    .padding(.vertical)
+                    
+                    Spacer()
+                    
+                    // MARK: Sign in button
                     Button {
                         
                     } label: {
-                        Text("Forgot Password ?")
-                            .font(.system(size: 13, weight: .semibold))
-                            .foregroundColor(.white)
-                            .padding(.top)
-                    }
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-                }
-                .padding(.horizontal)
-                .padding(.top, 12)
-                                
-                // MARK: Social sign in view
-                VStack {
-                    // Dividers + text
-                    HStack(spacing: 24) {
-                        Rectangle()
-                            .frame(width: 76, height: 1)
-                            .foregroundColor(.white)
-                            .opacity(0.5)
-                        
-                        Text("Sign in with social")
-                            .foregroundColor(.white)
-                            .fontWeight(.semibold)
-                        
-                        Rectangle()
-                            .frame(width: 76, height: 1)
-                            .foregroundColor(.white)
-                            .opacity(0.5)
-                    }
-                    // Sign up buttons
-                    HStack(spacing: 24) {
-                        Button {
+                        HStack {
+                            Text("SIGN IN")
+                                .foregroundColor(.black)
                             
-                        } label: {
-                            Image("facebook-sign-in-icon")
-                                .resizable()
-                                .frame(width: 44, height: 44)
+                            Image(systemName: "arrow.right")
+                                .foregroundColor(.black)
                         }
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 50)
+                    }
+                    .background(Color.white)
+                    .cornerRadius(10)
 
-                        Button {
+                    
+                    // MARK: Sign up button
+                    Spacer()
+                    
+                    NavigationLink {
+                        RegistrationView()
+                            .navigationBarBackButtonHidden(true)
+                    } label: {
+                        HStack {
+                            Text("Dont't have an account ?")
+                                .font(.system(size: 14))
                             
-                        } label: {
-                            Image("google-sign-in-icon")
-                                .resizable()
-                                .frame(width: 44, height: 44)
+                            Text("Sign Up")
+                                .font(.system(size: 14, weight: .semibold))
                         }
+                        .foregroundColor(.white)
                     }
-                }
-                .padding(.vertical)
-                
-                Spacer()
-                
-                // MARK: Sign in button
-                Button {
-                    
-                } label: {
-                    HStack {
-                        Text("SIGN IN")
-                            .foregroundColor(.black)
-                        
-                        Image(systemName: "arrow.right")
-                            .foregroundColor(.black)
-                    }
-                    .frame(width: UIScreen.main.bounds.width - 32, height: 50)
-                }
-                .background(Color.white)
-                .cornerRadius(10)
-
-                
-                // MARK: Sign up button
-                Spacer()
-                
-                Button {
-                    
-                } label: {
-                    HStack {
-                        Text("Dont't have an account ?")
-                            .font(.system(size: 14))
-                        
-                        Text("Sign Up")
-                            .font(.system(size: 14, weight: .semibold))
-                    }
-                    .foregroundColor(.white)
                 }
             }
         }
